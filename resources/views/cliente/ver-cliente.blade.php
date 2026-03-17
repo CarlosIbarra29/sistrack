c2b35cde372da63a3fed9dd285927952972adb95
Wrudng <imdiegog1234@outlook.com>
2026-03-17 12:57:46 -0700

    pull proyecto

--- resources/views/cliente/ver-cliente.blade.php
+++ resources/views/cliente/ver-cliente.blade.php
@@ -113,6 +113,22 @@
                                     </div>
                                 </div>
                             </div>
+
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <label class="text-muted">Servicio con arma</label>
+                                    <div class="font-weight-bold">
+                                        ${{ number_format($data->servicio_arma, 2) }}
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6">
+                                    <label class="text-muted">Servicio sin arma</label>
+                                    <div class="font-weight-bold">
+                                        ${{ number_format($data->servicio_sin_arma, 2) }}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
