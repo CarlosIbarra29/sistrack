4b9c83e05b4078244e5e81b3cee25a031599420f
Ariadna <ari.zfp03@gmail.com>
2026-03-17 12:44:51 -0600

    pull proyecto

--- resources/views/programacion/agregar-programacion.blade.php
+++ resources/views/programacion/agregar-programacion.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @push('scripts')
-    <script src="{{ asset('js/programacion/AgregarProgramacion.js?v=' . time()) }}"></script>
+    <script src="{{ asset('js/programacion/AgregarProgramacion.js') }}"></script>
 @endpush
 
 @section('title')
@@ -71,11 +71,12 @@
                                     <input type="datetime-local" class="form-control form-control-lg"
                                            name="fecha_hora" id="fecha_hora" required>
                                 </div>
+
                             </div>
 
                             <div class="form-group row">
 
-                                <div class="col-lg-2">
+                                <div class="col-lg-3">
                                     <label class="font-weight-bold">Tipo de servicio</label>
                                     <div class="radio-inline mt-2">
                                         <label class="radio">
@@ -89,19 +90,7 @@
                                     </div>
                                 </div>
 
-                            <div class="col-lg-2"> <label class="font-weight-bold">Armado</label>
-                                    <div class="radio-inline mt-2">
-                                        <label class="radio">
-                                            <input type="radio" checked name="armado_servicio" value="0">
-                                            <span></span> Si
-                                        </label>
-                                        <label class="radio ml-4"> <input type="radio" name="armado_servicio" value="1">
-                                            <span></span> No
-                                        </label>
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-2">
+                                <div class="col-lg-3">
                                     <label class="font-weight-bold">Monitoreo</label>
                                     <div class="radio-inline mt-2">
                                         <label class="radio">
@@ -120,112 +109,116 @@
                                     <select class="form-control form-control-lg"
                                             id="id_tarifa"
                                             name="id_tarifa"
-                                            required></select>                                                                                                                                                                                 
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                    <div class="col-lg-6"> 
-                                        <label class="font-weight-bold">Folio Interno</label>
-                                        <input type="text" class="form-control form-control-lg" 
-                                               name="folio_interno" id="folio_interno" required>
-                                    </div              <div class= <div class="bg-light rounded p-6 mb-8 border">
-
-                            <h5 class="font-weight-bold text-dark mb-6">Asignación de custodio</h5>
-
-                            <div class="form-group row">
-
-                                <div class="col-lg-6">
-                                    <label class="font-weight-bold">Custodio</label>
-                                    <select class="form-control form-control-lg"
-                                            id="custodio_id"
-                                            name="custodio_id"
-                                            required>
-                                        <option value="">Selecciona un custodio</option>
-                                        @foreach($custodio as $cli)
-                                            <option value="{{ $cli->id }}">
-                                                {{ $cli->nombre_custodio }} 
-                                                {{ $cli->ap_paterno }} 
-                                                {{ $cli->ap_materno }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
-                                <div class="col-lg-6">
-                                    <label class="font-weight-bold">Acompañantes</label>
-                                    <div class="radio-inline mt-2">
-                                        <label class="radio">
-                                            <input type="radio" name="op_custodios" value="0">
-                                            <span></span> Si
-                                        </label>
-                                        <label class="radio">
-                                            <input type="radio" checked name="op_custodios" value="1">
-                                            <span></span> No
-                                        </label>
-                                    </div>
+                                            required></select>
                                 </div>
 
                             </div>
                         </div>
 
-                        <!-- ================= ACOMPAÑANTES DINÁMICOS ================= -->
-                        <div class="bg-light rounded p-6 border"
-                             id="div_custodios"
-                             style="display:none;">
+                        <!-- ================= RUTAS ================= -->
+                        <div class="bg-light rounded p-6 mb-8 border">
 
-                            <div class="d-flex justify-content-between align-items-center mb-6">
-                                <h5 class="font-weight-bold text-dark mb-0">Acompañantes</h5>
+                            <h5 class="font-weight-bold text-dark mb-6">Rutas del servicio</h5>
 
-                                <a href="#"
-                                   class="btn btn-outline-warning btn-sm hrefAgregarOtro">
-                                    <i class="flaticon2-plus"></i> Agregar
-                                </a>
+                            <div class="form-group row">
+                                <div class="col-lg-6">
+                                    <label class="font-weight-bold">Domicilio origen</label>
+                                    <input type="text" class="form-control form-control-lg"
+                                           name="dom_origen" id="dom_origen" required>
+                                </div>
+
+                                <div class="col-lg-6">
+                                    <label class="font-weight-bold">Domicilio destino</label>
+                                    <input type="text" class="form-control form-control-lg"
+                                           name="dom_destino" id="dom_destino" required>
+                                </div>
                             </div>
+                        </div>
 
-                            <div class="table-responsive">
-                                <table class="table table-head-custom table-bordered table-hover"
-                                       id="tblDocumentos">
-                                    <thead class="thead-light">
-                                        <tr>
-                                            <th>Custodio</th>
-                                            <th>Opción</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody id="tbodyDocumentos"></tbody>
-                                </table>
-                            </div>: 50px;">Opción</th>
+                        <!-- ================= ASIGNACIÓN ================= -->
+                        
+                         <div class="form-group row">
+                                    <div class="col-lg-6">
+                                        <label>Custodio</label>
+                                        <select class="form-control" id="custodio_id" name="custodio_id" required >
+                                            <option value="">Selecciona una custodio</option>
+                                            @foreach($custodio as $cli)
+                                                <option value="{{ $cli->id }}" >{{ $cli->nombre_custodio }} {{ $cli->ap_paterno }} {{ $cli->ap_materno }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                    <div class="col-lg-6">
+                                        <label>Acompañantes</label>
+                                        <div class="radio-inline">
+                                            <label class="radio">
+                                                <input type="radio"  name="op_custodios" id="op_c_uno" value="0" />
+                                                <span></span>
+                                                Si
+                                            </label>
+                                            <label class="radio">
+                                                <input type="radio" checked name="op_custodios" id="op_c_dos" value="1" />
+                                                <span></span>
+                                                No
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card card-custom gutter-b" id="div_custodios" style="background-color:  #f1f1f1; display: none;" >
+                                    <div class="card-header">
+                                        <div class="card-title">
+                                            <h3 class="card-label">
+                                                Acompañantes
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="row form-group" >
+                                            <div class="col-lg-12" id="tblArchivos">
+                                                <table class='table table-bordered table-hover' id='tblDocumentos'>
+                                                    <thead>
+                                                    <tr>
+                                                        <th>Custodio</th>
+                                                        <th>Opción</th>
                                                     </tr>
-                                                </thead>
-                                                <tbody id='tbodyDocumentos'>
-                                                </tbody>
-                                            </table>
+                                                    </thead>
+                                                    <tbody id='tbodyDocumentos'>
+                                                    </tbody>
+                                                </table>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <a href="#" class="btn btn-icon btn-outline-warning btn-circle btn-sm hrefAgregarOtro" 
-                                               data-toggle="tooltip" title="Agregar acompañante">
-                                                <i class="flaticon2-plus"></i>
-                                            </a>
+                                        <div class="row form-group">
+                                            <div class="col-lg-12">
+                                                <a href="#" class="btn btn-icon btn-outline-warning btn-circle btn-sm mr-2 hrefAgregarOtro" data-toggle="tooltip" data-theme="dark" title="Agregar archivo">
+                                                    <i class="flaticon2-plus"></i>
+                                                </a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+
+{{-- 
+
+                                <div class="form-group">
+                                    <div class="col-lg-12">
+                                        <label for="observaciones">Observaciones</label>
+                                        <textarea class="form-control" name="observaciones" placeholder="*Opcional" id="observaciones" rows="5"></textarea>
+                                    </div>
+                                </div> --}}
                             </div>
+
                         </div>
 
-                                <div class="bg-light rounded p-6 mb-8 border">
-                                    <h5 class="font-weight-bold text-dark mb-6">Observaciones</h5>
-                                    <div class="form-group row">
-                                        <div class="col-lg-12">
-                                            <textarea class="form-control" name="observaciones"id="observaciones"rows="4"style="resize: none;"></textarea>
-                                        </div>
-                                    </div>
-                                </div>
+                        </div>
 
-                                <div class="card-footer bg-white border-top text-right">
-                        <button type="button"  id="btnGuardar"
+                    </div>
+
+                    <!-- FOOTER MODERNO -->
+                    <div class="card-footer bg-white border-top text-right">
+                        <button type="button"
+                                id="btnGuardar"
                                 class="btn btn-warning font-weight-bold px-8 mr-2">
                             <i class="flaticon2-check-mark"></i> Guardar
                         </button>
@@ -235,9 +228,7 @@
                             Cancelar
                         </a>
                     </div>
-                    </div>
-             </div>
-        </div>
+
                 </form>
 
             </div>
